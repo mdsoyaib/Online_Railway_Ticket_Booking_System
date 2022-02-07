ffbcc871d0346de2bb0d6924bfc84d9f68ff2177
Soyaib <mdsoyaib2025@gmail.com>
2022-02-08 00:50:06 +0600

    first commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.views import View
+
+# Create your views here.
+
+class Home(View):
+    def get(self, request):
+        return render(request, 'home.html')
+
+
+class Login(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+
+class Signup(View):
+    def get(self, request):
+        return render(request, 'signup.html')
