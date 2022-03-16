24467cd58e637816c3e2a3e555983e613f02ca41
Soyaib <mdsoyaib2025@gmail.com>
2022-03-16 18:44:35 +0600

    small correction for cancel booking filter

--- app/templatetags/custom_filter_date.py
+++ app/templatetags/custom_filter_date.py
@@ -1,5 +1,6 @@
 from django import template
 from datetime import datetime, timezone
+from django.shortcuts import redirect
 
 
 register = template.Library()
@@ -8,10 +9,15 @@ register = template.Library()
 # this filter has been created to to show cancel booking button if remaining date more than 1 day
 @register.filter(name="date")
 def date(travel_dt, current_date):
-    result = travel_dt - current_date
-    result = str(result)
-    time = result.split()
-    x = time[0]
-    x = int(x)
+    try:
+        result = travel_dt - current_date
+        result = str(result)
+        time = result.split()
+        x = time[0]
+        x = int(x)
+        return x
 
-    return x
+    except:
+        return redirect('home')
+
+    
